e5dff1dc9bc30b5a742c2c3afdbbd51cd32efa3c
joskolenberg <joskolenberg@gmail.com>
2018-10-23 15:56:34 +0200

    Cleanup.

--- tests/JoryBuilders/AlbumJoryBuilder.php
+++ tests/JoryBuilders/AlbumJoryBuilder.php
@@ -3,7 +3,6 @@
 namespace JosKolenberg\LaravelJory\Tests\JoryBuilders;
 
 use JosKolenberg\Jory\Support\Sort;
-use JosKolenberg\Jory\Support\Filter;
 use JosKolenberg\LaravelJory\JoryBuilder;
 
 class AlbumJoryBuilder extends JoryBuilder
--- tests/JoryBuilders/InstrumentJoryBuilder.php
+++ tests/JoryBuilders/InstrumentJoryBuilder.php
@@ -2,7 +2,6 @@
 
 namespace JosKolenberg\LaravelJory\Tests\JoryBuilders;
 
-use JosKolenberg\Jory\Support\Filter;
 use JosKolenberg\LaravelJory\JoryBuilder;
 
 class InstrumentJoryBuilder extends JoryBuilder
