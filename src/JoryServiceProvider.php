6218cc342fdf8c6f3308679239b6083feadad9ab
joskolenberg <joskolenberg@gmail.com>
2018-11-13 12:48:37 +0100

    Added publishing for config.

--- src/JoryServiceProvider.php
+++ src/JoryServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class JoryServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/jory.php' => config_path('jory.php'),
+        ]);
+    }
+
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/jory.php', 'jory');
