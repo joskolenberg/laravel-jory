973d8ca8d721c20cc6f1410e7b1d8fdb82f5a191
joskolenberg <joskolenberg@gmail.com>
2018-11-13 17:38:02 +0100

    Fixed routes.

--- src/Routes/BuildsJoryRoutes.php
+++ src/Routes/BuildsJoryRoutes.php
@@ -9,13 +9,13 @@ trait BuildsJoryRoutes
     public static function routes(string $baseUri = '')
     {
         // Base routes
-        Route::get($baseUri, JoryController::class.'@multiple');
-        Route::options($baseUri, JoryController::class.'@resourceList');
+        Route::get($baseUri, '\\'.JoryController::class.'@multiple');
+        Route::options($baseUri, '\\'.JoryController::class.'@resourceList');
 
         // Routes by resource
-        Route::get($baseUri.'/{uri}/count', JoryController::class.'@count');
-        Route::get($baseUri.'/{uri}/{id}', JoryController::class.'@show');
-        Route::get($baseUri.'/{uri}', JoryController::class.'@index');
-        Route::options($baseUri.'/{uri}', JoryController::class.'@options');
+        Route::get($baseUri.'/{uri}/count', '\\'.JoryController::class.'@count');
+        Route::get($baseUri.'/{uri}/{id}', '\\'.JoryController::class.'@show');
+        Route::get($baseUri.'/{uri}', '\\'.JoryController::class.'@index');
+        Route::options($baseUri.'/{uri}', '\\'.JoryController::class.'@options');
     }
 }
