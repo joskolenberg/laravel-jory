42166bde1abf192af8542bd87b98fcf87524403b
joskolenberg <joskolenberg@gmail.com>
2019-06-11 11:52:23 +0200

    Improved Jory Call Exception.

--- src/Exceptions/LaravelJoryCallException.php
+++ src/Exceptions/LaravelJoryCallException.php
@@ -7,7 +7,7 @@ namespace JosKolenberg\LaravelJory\Exceptions;
  * because of invalid data passed in the jory query.
  *
  * These exceptions will be used to show the errors in the return data
- * and will result into a 404 to the user by the JoryHandler.
+ * and will result into a 422 to the user by the JoryHandler.
  *
  * Class LaravelJoryCallException
  */
@@ -15,10 +15,11 @@ class LaravelJoryCallException extends \Exception
 {
     protected $errors = [];
 
-    public function __construct(array $errors, string $message = '', int $code = 0, \Throwable $previous = null)
+    public function __construct(array $errors)
     {
         $this->errors = $errors;
-        parent::__construct($message, $code, $previous);
+
+        parent::__construct(implode(', ', $errors), 0, null);
     }
 
     /**
