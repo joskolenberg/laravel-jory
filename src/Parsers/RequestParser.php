54b116be25a98512129c32c310930dd026c9df24
joskolenberg <joskolenberg@gmail.com>
2019-10-24 12:45:45 +0200

    Added base64 support

--- src/Parsers/RequestParser.php
+++ src/Parsers/RequestParser.php
@@ -45,6 +45,25 @@ class RequestParser implements JoryParserInterface
             return (new ArrayParser($data))->getJory();
         }
 
+        if ($this->dataIsBase64($data)) {
+            $data = base64_decode($data);
+        }
+
         return (new JsonParser($data))->getJory();
+    }
+
+    /**
+     * Check if the data is base64 encoded.
+     *
+     * Might not tell if it's valid but works well enough
+     * to make the distinction between base64 and json.
+     * And that's all that we need to know here.
+     *
+     * @param $data
+     * @return bool
+     */
+    function dataIsBase64($data)
+    {
+        return (bool) preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $data);
     }
 }
---
+++ tests/Base64Test.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace JosKolenberg\LaravelJory\Tests;
+
+class Base64Test extends TestCase
+{
+
+    /** @test */
+    public function it_can_process_a_base64_encoded_json_string()
+    {
+        $response = $this->json('GET', 'jory/band', [
+            'jory' => base64_encode('{"filter":{"f":"name","o":"like","d":"%zep%"},"rlt":{"albums":{"flt":{"f":"name","o":"like","d":"%III%"}}},"fld":["id","name"]}'),
+        ]);
+
+        $response->assertStatus(200)->assertExactJson([
+            'data' => [
+                [
+                    'id' => 2,
+                    'name' => 'Led Zeppelin',
+                    'albums' => [
+                        [
+                            'id' => 6,
+                            'band_id' => 2,
+                            'name' => 'Led Zeppelin III',
+                            'release_date' => '1970-10-05 00:00:00',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertQueryCount(2);
+    }
+
+    /** @test */
+    public function it_can_process_a_base64_encoded_json_string_2()
+    {
+        $response = $this->json('GET', 'jory/band', [
+            'jory' => 'eyJmaWx0ZXIiOnsiZiI6Im5hbWUiLCJvIjoibGlrZSIsImQiOiIlaW4lIn0sInJsdCI6eyJzb25ncyI6eyJmbGQiOlsiaWQiLCJ0aXRsZSJdLCJmbHQiOnsiZiI6InRpdGxlIiwibyI6Imxpa2UiLCJkIjoiJWxvdmUlIn0sInJsdCI6eyJhbGJ1bSI6e319fX19',
+        ]);
+
+        $response->assertStatus(200)->assertExactJson([
+            'data' => [
+                [
+                    'id' => 1,
+                    'name' => 'Rolling Stones',
+                    'year_start' => 1962,
+                    'year_end' => null,
+                    'songs' => [
+                        [
+                            'id' => 2,
+                            'title' => 'Love In Vain (Robert Johnson)',
+                            'album' => [
+                                'id' => 1,
+                                'band_id' => 1,
+                                'name' => 'Let it bleed',
+                                'release_date' => '1969-12-05 00:00:00',
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Led Zeppelin',
+                    'year_start' => 1968,
+                    'year_end' => 1980,
+                    'songs' => [
+                        [
+                            'id' => 47,
+                            'title' => 'Whole Lotta Love',
+                            'album' => [
+                                'id' => 5,
+                                'band_id' => 2,
+                                'name' => 'Led Zeppelin II',
+                                'release_date' => '1969-10-22 00:00:00',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertQueryCount(3);
+    }
+
+
+}
